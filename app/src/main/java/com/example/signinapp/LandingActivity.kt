9b907674d6deee233ab7aaba5787fc581a343d4b
12cookie <amciitd2020@gmail.com>
2024-08-19 12:15:15 +0530

    Text to speech

--- app/src/main/java/com/example/signinapp/LandingActivity.kt
+++ app/src/main/java/com/example/signinapp/LandingActivity.kt
@@ -2,7 +2,12 @@ package com.example.signinapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,6 +18,29 @@ class LandingActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_landing)
+
+        val languages = resources.getStringArray(R.array.Languages)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null)
+        {
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languages)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener
+                {
+                    override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long)
+                    {
+                        Toast.makeText(this@LandingActivity, getString(R.string.selected_language) + " " +
+                                "" + languages[position], Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>)
+                    {
+                    // write code to perform some action
+                    }
+                }
+        }
 
         val buttonClick1 = findViewById<Button> (R.id.logout)
         buttonClick1.setOnClickListener {
